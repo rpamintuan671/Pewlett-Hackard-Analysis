fe15445740751c69eeba2bfee4b5c07f1deb1dbc
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-08-12 14:42:46 -0700

    Retirement Info Added

--- schema.sql
+++ schema.sql
@@ -5,6 +5,28 @@ CREATE TABLE departments (
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+SELECT * FROM employees
+SELECT * FROM dept_emp
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
 -- Create tables for employees
 CREATE TABLE employees(
      emp_no INT NOT NULL,
@@ -15,15 +37,7 @@ CREATE TABLE employees(
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 ); 
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
+
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -32,20 +46,18 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments;
 
-CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no)
-);
 CREATE TABLE titles (
-emp_no INT NOT NULL,
+  emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM dept_manager
